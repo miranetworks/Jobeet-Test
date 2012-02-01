c7b9e8fab8f8a3108539a72030f3689f652433be
Ian Janse van Rensburg <ianjvr@miranetworks.net>
2012-02-01 13:00:05 +0200

    jobeet update

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,31 @@ set :application, "Jobeet"
 set :domain,      "testing-deploy"
 
 #deploy dir
-set :deploy_to,   "/var/home/ianjvr/#{domain}"
+set :deploy_to,   "/home/ianjvr/mira/#{domain}"
+set :app_path,    "app"
+set :web_path,    "web"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+#User settings:
+set :user,        "ianjvr"
+set :use_sudo,    false
+ssh_options[:port] = 22
+
+#GIT repository
+set :repository,  "git@github.com:ianjvr/Jobeet-Test.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch,      "master"
+
+
+#Deploy settings
+set :model_manager, "propel"
+#set   :deploy_via,    :copy
+set :deploy_via,  :rsync_with_remote_cache
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
+#tweaks
 set  :keep_releases,  3
+set :cache_warmup, false
